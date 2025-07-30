12d12ebf856974e0c8e4714a726c1ba27a4fa648
Miko-Adrian <cakipoko.exe@gmail.com>
2025-07-30 10:41:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ if abs(a) < 1e-6:
     st.stop()
 
 # Plot kurva lebih kecil
-fig, ax = plt.subplots(figsize=(3, 3))
+fig, ax = plt.subplots(figsize=(2.5, 2.5))
 x_fit = np.linspace(0, df["Konsentrasi"].max() * 1.1, 100)
 y_fit = a * x_fit + b
 
